490ee1389a8da2e9b9d515b1997a398b362fdbf3
smilejakdu <ash982416@gmail.com>
2021-08-16 10:14:32 +0900

    4-4 ParseIntPipe , ParseArrayPipe , PickType

--- src/workspaces/workspaces.controller.ts
+++ src/workspaces/workspaces.controller.ts
@@ -1,31 +1,43 @@
-import { Controller, Delete, Get, Post } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Body, Controller, Delete, Get, Post } from "@nestjs/common";
+import { ApiOperation, ApiTags } from "@nestjs/swagger";
+import { User } from "../decorators/user.decorator";
+import { Users } from "../entities/Users";
+import { CreateWorkspaceDto } from "./dto/create-workspace.dto";
+import { WorkspacesService } from "./workspaces.service";
 
-
-@ApiTags('WORKSPACE')
-@Controller('api/workspaces')
+@ApiTags("WORKSPACE")
+@Controller("api/workspaces")
 export class WorkspacesController {
-	@Get()
-	getMyWorkspaces() {
+  constructor(private workspacesService: WorkspacesService) {}
 
-	}
-	@Post()
-	createWorkspace() {
+  @ApiOperation({ summary: "내 워크스페이스 가져오기" })
+  @Get()
+  async getMyWorkspaces(@User() user: Users) {
+    return this.workspacesService.findMyWorkspaces(user.id);
+  }
 
-	}
+  @ApiOperation({ summary: "워크스페이스 만들기" })
+  @Post()
+  async createWorkspace(@User() user: Users, @Body() body: CreateWorkspaceDto) {
+    // body 는 dto 로 만들어줘야한다
+    return this.workspacesService.createWorkspace(
+      body.workspace,
+      body.url,
+      user.id,
+    );
+  }
 
-	@Get(':url/members')
-	getAllMembersFromWorkspace() {
-	// 네이밍을 할때 길게 적더라도 직관적이게 적어주는것이 좋다.
+  @Get(":url/members")
+  getAllMembersFromWorkspace() {
+    // 네이밍을 할때 길게 적더라도 직관적이게 적어주는것이 좋다.
+  }
 
-	}
+  @Post(":url/members")
+  inviteMembersToWorkspace() {}
 
-	@Post(':url/members')
-	inviteMembersToWorkspace() { }
-	
-	@Delete(':url/members/:id')
-	kickMemberFromWorkspace() { }
+  @Delete(":url/members/:id")
+  kickMemberFromWorkspace() {}
 
-	@Get(':url/members/:id')
-	getMemberInfoInWorkspace() { }
+  @Get(":url/members/:id")
+  getMemberInfoInWorkspace() {}
 }
--- src/workspaces/workspaces.service.ts
+++ src/workspaces/workspaces.service.ts
@@ -1,4 +1,55 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+import { ChannelMembers } from "../entities/ChannelMembers";
+import { Channels } from "../entities/Channels";
+import { Users } from "../entities/Users";
+import { WorkspaceMembers } from "../entities/WorkspaceMembers";
+import { Workspaces } from "../entities/Workspaces";
 
 @Injectable()
-export class WorkspacesService {}
+export class WorkspacesService {
+  constructor(
+    @InjectRepository(Workspaces)
+    private workspacesRepository: Repository<Workspaces>,
+    @InjectRepository(Channels)
+    private channelsRepository: Repository<Channels>,
+    @InjectRepository(WorkspaceMembers)
+    private workspaceMembersRepository: Repository<WorkspaceMembers>,
+    @InjectRepository(ChannelMembers)
+    private channelMembersRepository: Repository<ChannelMembers>,
+    @InjectRepository(Users)
+    private usersRepository: Repository<Users>,
+  ) {}
+  async findById(id: number) {
+    return this.workspacesRepository.findOne({ where: { id } });
+  }
+
+  async findMyWorkspaces(myId: number) {
+    return this.workspacesRepository.find({
+      where: {
+        WorkspaceMembers: [{ userId: myId }],
+      },
+    });
+  }
+
+  async createWorkspace(name: string, url: string, myId: number) {
+    const workspace = new Workspaces();
+    workspace.name = name;
+    workspace.url = url;
+    workspace.OwnerId = myId;
+    const returned = await this.workspacesRepository.save(workspace);
+    const workspaceMember = new WorkspaceMembers();
+    workspaceMember.UserId = myId;
+    workspaceMember.WorkspaceId = returned.id;
+    await this.workspaceMembersRepository.save(workspaceMember);
+    const channel = new Channels();
+    channel.name = "일반";
+    channel.WorkspaceId = returned.id;
+    const channelReturned = await this.channelsRepository.save(channel);
+    const channelMember = new ChannelMembers();
+    channelMember.UserId = myId;
+    channelMember.ChannelId = channelReturned.id;
+    await this.channelMembersRepository.save(channelMember);
+  }
+}
